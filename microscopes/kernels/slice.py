b50c3ca96f13f90dc1a5432447f48cec9b50eb16
Stephen Tu <stephen@qadium.com>
2014-06-19 11:26:02 -0700

    slice sampler base kernel

---
+++ microscopes/kernels/slice.py
@@ -0,0 +1,58 @@
+"""
+Slice sampler based on:
+    http://projecteuclid.org/download/pdf_1/euclid.aos/1056562461
+    https://github.com/ericmjonas/netmotifs/blob/master/irm/slicesample.cc
+"""
+
+import numpy as np
+
+import math
+import logging
+logger = logging.getLogger(__name__)
+
+def interval(pdf, x0, y, w, m):
+    """
+    Fig. 3 of http://projecteuclid.org/download/pdf_1/euclid.aos/1056562461
+    """
+    U = np.random.random()
+    L = x0 - w*U
+    R = L + w
+    V = np.random.random()
+    J = int(math.floor(m*V))
+    K = m-1-J
+
+    while J > 0 and y < pdf(L):
+        L -= w
+        J -= 1
+
+    while K > 0 and y < pdf(R):
+        R += w
+        K -= 1
+
+    if J == 0 or K == 0:
+        logging.warn('interval hit maximum expansions')
+    return L, R
+
+def shrink(pdf, x0, y, L, R):
+    """
+    Fig. 5 of http://projecteuclid.org/download/pdf_1/euclid.aos/1056562461
+    """
+    ntries = 100
+    while ntries:
+        U = np.random.random()
+        x1 = L + U*(R-L)
+        if y < pdf(x1):
+            return x1
+        if x1 < x0:
+            L = x1
+        else:
+            R = x1
+        ntries -= 1
+
+    logging.warn('shrink exceeded maximum iterations (%d)' % (ntries))
+    return x1
+
+def slicesample(pdf, x0, w):
+    y = np.log(np.random.random()) + pdf(x0)
+    L, R = interval(pdf, x0, y, w, 1000)
+    return shrink(pdf, x0, y, L, R)
---
+++ test/test_kernel_slice.py
@@ -0,0 +1,34 @@
+from scipy.stats import norm
+from microscopes.kernels.slice import slicesample
+
+import numpy as np
+
+def kl(a, b, dA):
+    return np.sum([p*np.log(p/q)*dA for p, q in zip(a, b)])
+
+def hist(data, bins):
+    H, _ = np.histogram(data, bins=bins, density=False)
+    return H
+
+def test_gauss():
+    # sample from N(0,1)
+
+    pdf = lambda x: norm.logpdf(x)
+    def sampler(x0, niters):
+        x = x0
+        for _ in xrange(niters):
+            x = slicesample(pdf, x, 0.1)
+            yield x
+
+    bins = np.linspace(-3, 3, 1000)
+    smoothing = 1e-5
+
+    actual_samples = np.random.normal(size=1000)
+    actual_hist = hist(actual_samples, bins) + smoothing
+    actual_hist /= actual_hist.sum()
+
+    slice_samples = np.array(list(sampler(1.0, 1000)))
+    slice_hist = hist(slice_samples, bins) + smoothing
+    slice_hist /= slice_hist.sum()
+
+    assert kl(actual_hist, slice_hist, bins[1]-bins[0]) <= 0.1
