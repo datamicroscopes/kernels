143a17159d9d8d013fdc2c791bdd8474e00207da
Stephen Tu <stephen@qadium.com>
2014-06-29 03:04:43 -0700

    gibbs_assign with missing data

--- test/test_kernel_gibbs_assign.py
+++ test/test_kernel_gibbs_assign.py
@@ -23,6 +23,7 @@ from microscopes.py.models import bbnc
 import itertools as it
 import math
 import numpy as np
+import numpy.ma as ma
 from scipy.misc import logsumexp
 
 from nose.plugins.attrib import attr
@@ -65,9 +66,15 @@ def cluster(Y, assignments):
         if assign not in labels:
             labels[assign] = len(labels)
     clusters = [[] for _ in xrange(len(labels))]
+    masks = [[] for _ in xrange(len(labels))] if hasattr(Y, 'mask') else None
     for ci, yi in zip(assignments, Y):
         clusters[labels[ci]].append(yi)
-    return tuple(np.array(c) for c in clusters)
+        if masks is not None:
+            masks[labels[ci]].append(yi.mask)
+    if masks is None:
+        return tuple(np.array(c) for c in clusters)
+    else:
+        return tuple(ma.array(np.array(c), mask=m) for c, m in zip(clusters, masks))
 
 def _test_convergence_simple(
     N,
@@ -75,9 +82,11 @@ def _test_convergence_simple(
     cxx_models,
     clusterhp,
     featurehps,
+    preprocess_data_fn=None,
     burnin_niters=10000,
     nsamples=1000,
     skip=10,
+    attempts=3,
     threshold=0.01):
 
     assert len(py_models) == len(cxx_models)
@@ -94,6 +103,10 @@ def _test_convergence_simple(
     Y = np.hstack(Y_clustered)
     assert Y.shape[0] == N
 
+    # preprocess the data (e.g. add masks)
+    if preprocess_data_fn:
+        Y = preprocess_data_fn(Y)
+
     # brute force the posterior of the actual model
     py_brute_s = make_dp(py_state, N, py_models, clusterhp, featurehps)
 
@@ -101,7 +114,8 @@ def _test_convergence_simple(
     # brute force the posterior of the actual model
     def posterior(assignments):
         py_brute_s.reset()
-        fill(py_brute_s, cluster(Y, assignments))
+        data = cluster(Y, assignments)
+        fill(py_brute_s, data)
         return py_brute_s.score_joint()
     actual_scores = np.array(map(posterior, permutation_iter(N)))
     actual_scores -= logsumexp(actual_scores)
@@ -128,7 +142,7 @@ def _test_convergence_simple(
         smoothing = 1e-5
         gibbs_scores = np.zeros(len(actual_scores)) + smoothing
 
-        outer = 3
+        outer = attempts
         last_kl = None
         while outer > 0:
             # now grab nsamples samples, every skip iters
@@ -166,99 +180,20 @@ def test_convergence_bb():
         clusterhp={'alpha':2.0},
         featurehps=[{'alpha':1.0,'beta':1.0}]*D)
 
-#def _test_mixture_model_convergence(
-#        mm,
-#        actual_mm,
-#        N,
-#        all_possible_assignments_fn,
-#        canonical_fn,
-#        kernel_fn,
-#        preprocess_data_fn=None,
-#        burnin_niters=10000,
-#        nsamples=1000,
-#        skip=10,
-#        threshold=0.1):
-#    Y_clustered, _ = mm.sample(N)
-#    Y = np.hstack(Y_clustered)
-#    assert Y.shape[0] == N
-#    actual_mm.fill(Y_clustered)
-#
-#    idmap = { C : i for i, C in enumerate(all_possible_assignments_fn(N)) }
-#
-#    # brute force the posterior of the actual model
-#    def posterior(assignments):
-#        actual_mm.reset()
-#        actual_mm.fill(cluster(Y, assignments))
-#        return actual_mm.score_joint()
-#    actual_scores = np.array(map(posterior, all_possible_assignments_fn(N)))
-#    actual_scores -= sp.misc.logsumexp(actual_scores)
-#    actual_scores = np.exp(actual_scores)
-#
-#    if preprocess_data_fn:
-#        Yp = preprocess_data_fn(Y)
-#    else:
-#        Yp = Y
-#    dataset = numpy_dataview(Yp)
-#    mm.bootstrap(dataset.data(shuffle=False))
-#
-#    # burnin
-#    for _ in xrange(burnin_niters):
-#        kernel_fn(mm, dataset.data(shuffle=True))
-#
-#    print 'finished burnin of', burnin_niters, 'iters'
-#
-#    smoothing = 1e-5
-#    gibbs_scores = np.zeros(len(actual_scores)) + smoothing
-#
-#    outer = 3
-#    last_kl = None
-#    while outer > 0:
-#        # now grab nsamples samples, every skip iters
-#        for _ in xrange(nsamples):
-#            for _ in xrange(skip):
-#                kernel_fn(mm, dataset.data(shuffle=True))
-#            gibbs_scores[idmap[tuple(canonical_fn(mm.assignments()))]] += 1
-#        gibbs_scores /= gibbs_scores.sum()
-#        kldiv = KL_discrete(actual_scores, gibbs_scores)
-#        print 'actual:', actual_scores
-#        print 'gibbs:', gibbs_scores
-#        print 'kl:', kldiv
-#        if kldiv <= threshold:
-#            return
-#        if last_kl is not None and kldiv >= last_kl:
-#            print 'WARNING: KL is not making progress!'
-#            print 'last KL:', last_kl
-#            print 'cur KL:', kldiv
-#        last_kl = kldiv
-#        outer -= 1
-#        print 'WARNING: did not converge, trying', outer, 'more times'
-#
-#    assert False, 'failed to converge!'
-#
-#def test_dirichlet_fixed_convergence():
-#    N = 4
-#    D = 5
-#    K = 2
-#    ddmm, actual_ddmm = \
-#        make_dd(N, K, [bb]*D, {'alpha':2.0}, [{'alpha':1.0, 'beta':1.0}]*D), \
-#        make_dd(N, K, [bb]*D, {'alpha':2.0}, [{'alpha':1.0, 'beta':1.0}]*D)
-#    def all_possible_assignments_fn(N):
-#        return it.product(range(K), repeat=N)
-#    canonical_fn = lambda x: x
-#    _test_mixture_model_convergence(
-#            ddmm, actual_ddmm, N, all_possible_assignments_fn,
-#            canonical_fn, gibbs_assign_fixed)
-#
-#def test_dirichlet_process_convergence():
-#    N = 4
-#    D = 5
-#    dpmm, actual_dpmm = \
-#        make_dp(N, [bb]*D, {'alpha':2.0}, [{'alpha':1.0, 'beta':1.0}]*D), \
-#        make_dp(N, [bb]*D, {'alpha':2.0}, [{'alpha':1.0, 'beta':1.0}]*D)
-#    _test_mixture_model_convergence(
-#            dpmm, actual_dpmm, N, permutation_iter,
-#            permutation_canonical, gibbs_assign)
-#
+def test_convergence_bb_missing():
+    N = 4
+    D = 5
+    def preprocess_fn(Y):
+        masks = [tuple(j == (i % len(Y)) for j in xrange(D)) for i in xrange(len(Y))]
+        return ma.array(Y, mask=masks)
+    _test_convergence_simple(
+        N=N,
+        py_models=[py_bb]*D,
+        cxx_models=[cxx_bb]*D,
+        clusterhp={'alpha':2.0},
+        featurehps=[{'alpha':1.0,'beta':1.0}]*D,
+        preprocess_data_fn=preprocess_fn)
+
 #@attr('slow')
 #def test_nonconj_inference():
 #    N = 1000
@@ -304,7 +239,7 @@ def test_convergence_bb():
 #    print 'inferred:', inferred
 #    print 'diff:', diff
 #    assert diff <= 0.2
-#
+
 #@attr('slow')
 #def test_nonconj_inference_kl():
 #    N = 2
@@ -321,21 +256,7 @@ def test_convergence_bb():
 #    _test_mixture_model_convergence(
 #            dpmm, actual_dpmm, N, permutation_iter,
 #            permutation_canonical, kernel_fn)
-#
-#def test_missing_data_inference_kl():
-#    N = 3
-#    D = 10
-#    dpmm, actual_dpmm = \
-#        make_dp(N, [bb]*D, {'alpha':2.0}, [{'alpha':1.0, 'beta':1.0}]*D), \
-#        make_dp(N, [bb]*D, {'alpha':2.0}, [{'alpha':1.0, 'beta':1.0}]*D)
-#    def preprocess_fn(Y):
-#        import numpy.ma as ma
-#        masks = [tuple(j == (i % len(Y)) for j in xrange(D)) for i in xrange(len(Y))]
-#        return ma.array(Y, mask=masks)
-#    _test_mixture_model_convergence(
-#            dpmm, actual_dpmm, N, permutation_iter,
-#            permutation_canonical, gibbs_assign, preprocess_fn)
-#
+
 #def test_different_datatypes():
 #    N = 10
 #    likelihoods = [bb, gp, nich, bb]
