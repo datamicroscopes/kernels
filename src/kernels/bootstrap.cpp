ccdecffddaef75b8d4a6f10aa58a619a2c27d71b
Stephen Tu <stephen@qadium.com>
2014-06-28 19:04:09 -0700

    use the new API names

--- src/kernels/bootstrap.cpp
+++ src/kernels/bootstrap.cpp
@@ -17,15 +17,9 @@ bootstrap::likelihood(state &s, dataview &view, rng_t &rng)
   for (view.next(); !view.end(); view.next()) {
     row_accessor acc = view.get();
     auto scores = s.score_value(acc, rng);
-    //cout << "  * scores: " << scores.second << endl;
     const auto choice = scores.first[util::sample_discrete_log(scores.second, rng)];
-    //cout << "  * probs: " << scores.second << endl;
-    //cout << "  * add to group " << choice << endl;
     s.add_value(choice, view, rng);
     if (choice == egid)
       egid = s.create_group(rng);
   }
-  //cout << "placed in " << s.ngroups() << endl;
 }
-
-
